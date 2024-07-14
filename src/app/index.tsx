126c641a77ea8d251b726086c39eea0582cdb0bd
EmersonFlorencio <emersonflorencio@hotmail.com>
2024-07-14 12:47:40 -0300

    chore: iniciando Projeto NLW Planner e realizando as configurações iniciais

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text} from "react-native"
+
+export default function Index() {
+  return (
+    <View className="bg-red-400">
+      <Text>
+        Hello Wolrd!
+      </Text>
+    </View>
+  );
+}

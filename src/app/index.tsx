5aea0cb3604ac3be608281f94fb911594b258678
EmersonFlorencio <emersonflorencio@hotmail.com>
2024-07-14 13:44:23 -0300

    feat: Criando a pasta components e realizando as configurações do google fonts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,9 +2,9 @@ import { View, Text} from "react-native"
 
 export default function Index() {
   return (
-    <View className="bg-red-400">
-      <Text>
-        Hello Wolrd!
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-zinc-400">
+        Convide seus amigos e programe sua proxima viagem!
       </Text>
     </View>
   );
